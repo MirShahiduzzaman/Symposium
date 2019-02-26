e7b8c7eedd75a9e9021d38f4ff6652b4851057e0
Mir Shahiduzzaman <43242843+MirShahiduzzaman@users.noreply.github.com>
2019-02-26 09:00:45 -0500

    setup

---
+++ platformerGame/Assets/walk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour
+{
+
+    private void OnMouseUp()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+
+
+    }
+
+    private void OnMouseEnter()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        print("hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("hello");
+
+    }
+}
